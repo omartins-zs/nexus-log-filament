d5ec3ab0487d980cf235c7f1b27bee6b89051feb
omartins-zs <gabrielmatheus209@gmail.com>
2026-05-25 15:15:14 -0300

    :sparkles: feat: adicionar criacao CD

---
+++ app/Filament/Resources/CentroDistribuicaos/Pages/CreateCentroDistribuicao.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\CentroDistribuicaos\Pages;
+
+use App\Filament\Resources\CentroDistribuicaos\CentroDistribuicaoResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateCentroDistribuicao extends CreateRecord
+{
+    protected static string $resource = CentroDistribuicaoResource::class;
+}
