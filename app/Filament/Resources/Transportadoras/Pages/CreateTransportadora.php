f2638249e04b39513c96c41237c84d7c49829a64
omartins-zs <gabrielmatheus209@gmail.com>
2026-05-25 15:15:24 -0300

    :sparkles: feat: adicionar criacao transportadora

---
+++ app/Filament/Resources/Transportadoras/Pages/CreateTransportadora.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Transportadoras\Pages;
+
+use App\Filament\Resources\Transportadoras\TransportadoraResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTransportadora extends CreateRecord
+{
+    protected static string $resource = TransportadoraResource::class;
+}
