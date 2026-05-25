54e62b1a6cb9fc25a7ef0e3d536e404ebcb382ff
omartins-zs <gabrielmatheus209@gmail.com>
2026-05-25 15:16:24 -0300

    :sparkles: feat: adicionar criacao recebimento

---
+++ app/Filament/Resources/Recebimentos/Pages/CreateRecebimento.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Recebimentos\Pages;
+
+use App\Filament\Resources\Recebimentos\RecebimentoResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRecebimento extends CreateRecord
+{
+    protected static string $resource = RecebimentoResource::class;
+}
