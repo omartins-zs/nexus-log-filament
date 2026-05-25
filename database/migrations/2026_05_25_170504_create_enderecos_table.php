bd94eee877f3aa120b619485ab864869972283c9
omartins-zs <gabrielmatheus209@gmail.com>
2026-05-25 15:13:53 -0300

    :card_file_box: data: criar tabela enderecos

---
+++ database/migrations/2026_05_25_170504_create_enderecos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('enderecos', function (Blueprint $table) {
+            $table->id();
+            $table->string('corredor');
+            $table->string('estante');
+            $table->string('nivel');
+            $table->string('codigo_barras')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('enderecos');
+    }
+};
